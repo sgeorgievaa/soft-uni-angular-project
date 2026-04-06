b732e4b9dcf68ccb6928ddb8faaa12b39a923ed5
sgeorgievaa <sevdalinag991@gmail.com>
2026-04-06 20:26:52 +0300

    Added routing

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home/home.component';
+import { CatalogComponent } from './features/catalog/catalog/catalog.component';
+import { DetailsComponent } from './features/herbs/details/details.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { CreateComponent } from './features/herbs/create/create.component';
+import { EditComponent } from './features/herbs/edit/edit.component';
+import { DashboardComponent } from './features/dashboard/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //public
+    { path: '', component: HomeComponent },
+    { path: 'catalog', component: CatalogComponent },
+    { path: 'catalog/:id', component: DetailsComponent },
+    
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    //private
+    { path: 'create', component: CreateComponent },
+    { path: 'edit/:id', component: EditComponent },
+    { path: 'dashboard', component: DashboardComponent },
+
+    { path: '**', redirectTo: '' }
+
+
+];
