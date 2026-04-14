cc19d9d51bb9a3e9660218bdbc350455c60672c9
sgeorgievaa <sevdalinag991@gmail.com>
2026-04-14 21:01:08 +0300

    Added user service with local storage

---
+++ project/src/app/features/auth/user.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { User } from '../../types/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private USERS_KEY = 'users';
+  private CURRENT_USER_KEY = 'currentUser';
+
+  constructor() { }
+
+  register(user: User): boolean {
+    const users = this.getUsers();
+  
+    const found = users.find(
+      u => u.email === user.email && u.password === user.password
+    );
+
+    if (found) return false;
+
+    users.push(user);
+    localStorage.setItem(this.USERS_KEY, JSON.stringify(users));
+
+    return true;
+  }
+
+  login(user: User): boolean {
+    const users = this.getUsers();
+
+    const found = users.find(
+      u => u.email === user.email && u.password === user.password
+    );
+
+    if (!found) return false;
+
+    localStorage.setItem(this.CURRENT_USER_KEY, JSON.stringify(found));
+    return true;
+  }
+
+  logout() {
+    localStorage.removeItem(this.CURRENT_USER_KEY);
+  }
+
+  getCurrentUser(): User | null {
+    const user = localStorage.getItem(this.CURRENT_USER_KEY);
+    return user ? JSON.parse(user) : null;
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getCurrentUser();
+  }
+
+
+  private getUsers(): User[] {
+    const users = localStorage.getItem(this.USERS_KEY);
+    return users ? JSON.parse(users) : [];
+  }
+}
