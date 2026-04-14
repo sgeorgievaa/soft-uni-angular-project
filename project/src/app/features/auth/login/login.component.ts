135fa853aa6219f5d44b083fee4d681ff85cd010
sgeorgievaa <sevdalinag991@gmail.com>
2026-04-14 21:21:55 +0300

    added login validation

--- project/src/app/features/auth/login/login.component.ts
+++ project/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  errorMessage = '';
 
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) return;
+
+    const { email, password } = form.value;
+
+    const success = this.userService.login({ email, password });
+
+    if (!success) {
+      this.errorMessage = 'Invalid email or password';
+      return;
+    }
+
+    this.router.navigate(['/']);
+  
+  }
 }
