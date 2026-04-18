411df6f07bf04d9af4bec589faa4403888aee1ba
sgeorgievaa <sevdalinag991@gmail.com>
2026-04-18 22:00:07 +0300

    Added create functionality

--- project/src/app/features/catalog/catalog/catalog.component.ts
+++ project/src/app/features/catalog/catalog/catalog.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Herb } from '../../../types/herb';
+import { HerbService } from '../../herbs/herb.service';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  herbs: Herb[] = [];
 
+  constructor(private herbService: HerbService) {}
+
+  ngOnInit(): void {
+    this.herbs = this.herbService.getAll();
+  }
 }
--- project/src/app/features/herbs/create/create.component.ts
+++ project/src/app/features/herbs/create/create.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HerbService } from '../herb.service';
+import { UserService } from '../../auth/user.service';
+import { Router } from '@angular/router';
+import { Herb } from '../../../types/herb';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  constructor(
+    private herbService: HerbService,
+    private authService: UserService,
+    private router: Router
+  ) {}
+
+  onSubmit(form: NgForm): void {
+    if(form.invalid) return;
+
+    const { name, benefits, usage, imageUrl } = form.value;
+
+    const currentUser = this.authService.getCurrentUser();
+
+    const newHerb: Herb = {
+      id: Date.now().toString(),
+      name,
+      benefits,
+      usage,
+      imageUrl,
+      ownerId: currentUser?.email || ''
+    };
+
+    this.herbService.create(newHerb);
+
+    form.reset();
+
+    this.router.navigate(['/catalog']);
+  }
 
 }
