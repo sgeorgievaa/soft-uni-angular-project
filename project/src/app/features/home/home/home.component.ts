abf6e343bd5a2d58e3ddd857e92f3c284ced37ee
sgeorgievaa <sevdalinag991@gmail.com>
2026-04-25 12:46:19 +0300

    Added slice on Home

--- project/src/app/features/home/home/home.component.ts
+++ project/src/app/features/home/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { Herb } from '../../../types/herb';
 import { HerbService } from '../../herbs/herb.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
