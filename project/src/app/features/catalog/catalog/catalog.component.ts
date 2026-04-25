da6ad94ef7632c8e5aef7e8cc86138ba21efa3ab
sgeorgievaa <sevdalinag991@gmail.com>
2026-04-25 11:15:12 +0300

    Added search in catalog

--- project/src/app/features/catalog/catalog/catalog.component.ts
+++ project/src/app/features/catalog/catalog/catalog.component.ts
@@ -2,20 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Herb } from '../../../types/herb';
 import { HerbService } from '../../herbs/herb.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
 export class CatalogComponent implements OnInit {
   herbs: Herb[] = [];
+  allHerbs: Herb[] = [];
+
+  searchTerm: string = '';
 
   constructor(private herbService: HerbService) {}
 
   ngOnInit(): void {
-    this.herbs = this.herbService.getAll();
+    this.allHerbs = this.herbService.getAll();
+    this.herbs = [...this.allHerbs];
+  }
+
+  onSearch(): void {
+    const term = this.searchTerm.toLowerCase();
+
+    this.herbs = this.allHerbs.filter(h => h.name.toLowerCase().includes(term));
   }
 }
